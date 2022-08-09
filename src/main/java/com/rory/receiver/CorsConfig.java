f7af62ea50c9957f03f3a6032ca4042beda3b0cb
HD0007 <47544051+HD0007@users.noreply.github.com>
2022-08-09 10:27:48 +0530

    Add files via upload

---
+++ src/main/java/com/rory/receiver/CorsConfig.java
@@ -0,0 +1,43 @@
+package com.rory.receiver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+// 
+/**
+ * The Class CorsConfig.
+ *
+ * @author icets
+ */
+@Configuration
+public class CorsConfig {
+
+	@Value("${spring.cors.allowedOriginPatterns}") 
+	private String allowedOriginPatterns;
+	
+	@Value("${spring.cors.allowedHeaders}") 
+	private String allowedHeader;
+	
+	@Value("${spring.cors.allowedMethods}") 
+	private String allowedMethod;
+   /**
+    * Cors filter.
+    *
+    * @return the cors filter
+    */
+   @Bean
+   public CorsFilter corsFilter() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(true);
+      config.addAllowedOriginPattern(allowedOriginPatterns);       
+      config.addAllowedHeader(allowedHeader);
+      config.addAllowedMethod(allowedMethod);
+      source.registerCorsConfiguration("/api/*", config);
+      return new CorsFilter(source);
+   }
+
+}
