ab529e0b1dedde3d0f6542ed88cb217846533ca3
HD0007 <47544051+HD0007@users.noreply.github.com>
2022-08-09 22:12:42 +0530

    Update CorsConfig.java

--- src/main/java/com/rory/receiver/CorsConfig.java
+++ src/main/java/com/rory/receiver/CorsConfig.java
@@ -1,43 +1,33 @@
 package com.rory.receiver;
-import org.springframework.beans.factory.annotation.Value;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-// 
-/**
- * The Class CorsConfig.
- *
- * @author icets
- */
-@Configuration
-public class CorsConfig {
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class CorsConfig extends WebSecurityConfigurerAdapter{
 
-	@Value("${spring.cors.allowedOriginPatterns}") 
-	private String allowedOriginPatterns;
-	
-	@Value("${spring.cors.allowedHeaders}") 
-	private String allowedHeader;
-	
-	@Value("${spring.cors.allowedMethods}") 
-	private String allowedMethod;
-   /**
-    * Cors filter.
-    *
-    * @return the cors filter
-    */
-   @Bean
-   public CorsFilter corsFilter() {
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
-      config.addAllowedOriginPattern(allowedOriginPatterns);       
-      config.addAllowedHeader(allowedHeader);
-      config.addAllowedMethod(allowedMethod);
-      source.registerCorsConfiguration("/**", config);
-      return new CorsFilter(source);
-   }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
