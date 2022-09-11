eabeae23ffda2b0edcf4c87a60885d6d9bb0cf3f
HD0007 <47544051+HD0007@users.noreply.github.com>
2022-09-11 17:44:15 +0530

    Update webSecurity.java

--- src/main/java/com/rory/receiver/webSecurity.java
+++ src/main/java/com/rory/receiver/webSecurity.java
@@ -1,16 +1,16 @@
-package com.rory.receiver;
+// package com.rory.receiver;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.web.cors.CorsConfiguration;
 
-@SuppressWarnings("deprecation")
-@EnableWebSecurity
-public class webSecurity extends WebSecurityConfigurerAdapter {
+// @SuppressWarnings("deprecation")
+// @EnableWebSecurity
+// public class webSecurity extends WebSecurityConfigurerAdapter {
 
-@Override
-protected void configure(HttpSecurity http) throws Exception {
-    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-}
-}
+// @Override
+// protected void configure(HttpSecurity http) throws Exception {
+//     http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+// }
+// }
